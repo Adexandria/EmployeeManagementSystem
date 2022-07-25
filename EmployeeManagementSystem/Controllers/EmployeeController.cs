cf8fea847f230f0a66dd518ddb27da8d0dbc36e7
gideon <edoghotugiddy@gmail.com>
2022-07-25 21:25:14 +0100

    Implemented the post and Delete in employeeController

--- EmployeeManagementSystem/Controllers/EmployeeController.cs
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -14,6 +14,7 @@ namespace EmployeeManagementSystem.Controllers
             _db = db;
         }
 
+        //POST
         [HttpPost]
         public async Task<IActionResult> Post([FromBody]Employee employee)
         {
