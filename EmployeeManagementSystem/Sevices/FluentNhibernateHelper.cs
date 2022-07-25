cd2bdd2e4041b11dc81a00546e998ca4d529fef1
gideon <edoghotugiddy@gmail.com>
2022-07-25 22:08:42 +0100

    Implemented the post and Delete in employeeController

--- EmployeeManagementSystem/Sevices/FluentNhibernateHelper.cs
+++ EmployeeManagementSystem/Sevices/FluentNhibernateHelper.cs
@@ -18,13 +18,28 @@ namespace EmployeeManagementSystem.Sevices
                 .Database(
                     MsSqlConfiguration.MsSql2008
                         .ConnectionString(
-                            @"Data Source=(localdb)\MSSQLLocalDB;Database=EmployeeDb;Integrated Security=True;")
+                            @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Desktop\Phase II\TutorialWithAdeola\EmployeeManagementSystem-1\EmployeeManagementSystem\EmployeeDB1.mdf;Integrated Security = True; Connect Timeout = 30")
                         .ShowSql()
                 )
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<EmployeeMap>())
                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false,true))
                 .BuildSessionFactory();
         }
-        
+       
+
+        /* public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2008
+                        .ConnectionString(
+                            @"Data Source=(localdb)\MSSQLLocalDB;Database=employeedb;Integrated Security=True;")
+                        .ShowSql()
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<EmployeeMap>())
+                .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(true,true))
+                .BuildSessionFactory();
+        }*/
+
     }
 }
